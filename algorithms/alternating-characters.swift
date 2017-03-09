cad7b788f5eb5ef0695bdf6923c39eaeb5529823
Gisela Su <giselasu17@gmail.com>
2017-03-09 01:05:07 -0500

    HackerRank challenge: Alternating Characters

--- algorithms/alternating-characters.swift
+++ algorithms/alternating-characters.swift
@@ -4,11 +4,11 @@ var n = Int(readLine()!)!
 for _ in 0..<n {
     var string = String(readLine()!)!
     var arraystring = string.characters.flatMap{String($0)}
-    var count = 0
+    var temp = 0
     for i in 0..<arraystring.count-1 {
         if arraystring[i] == arraystring[i+1] {
-            count += 1
+            temp += 1
         }
     }
-    print(count)
+    print(temp)
 }
