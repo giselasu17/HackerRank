74a92a71c0e1ba391d3b0a60f232de03fe0b619f
Gisela <giselasu17@yaismels-mbp.attlocal.net>
2017-03-09 00:51:44 -0500

    HackerRank: Funny String challenge

---
+++ algorithms/funny-string.swift
@@ -0,0 +1,28 @@
+import Foundation
+var n = Int(readLine()!)!
+var alf = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+var map = [String: Int]()
+for i in 0..<26 {
+    map[alf[i]] = i + 1;
+}
+
+for _ in 0..<n {
+    var string = String(readLine()!).characters.flatMap{String($0)}
+    var stringR = string
+    stringR = stringR.reversed()
+    var cont = 0
+    
+    for i in 0..<string.count-1 {
+        var str = Int(map[string[i+1]]!)-Int(map[string[i]]!)
+        var strR = Int(map[stringR[i+1]]!)-Int(map[stringR[i]]!)
+        if abs(str) == abs(strR) {
+            cont += 1
+        }
+    }
+    if cont == string.count-1 {
+        print("Funny")
+    }
+    else {
+        print("Not Funny")
+    }
+}
