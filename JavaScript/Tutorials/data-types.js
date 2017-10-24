23d57aa2189dec45a75f06a4ad12350fddad0dfe
giselasu17 <giselasu17@gmail.com>
2017-10-24 14:01:47 -0400

    HackerRank: Data Types Challenge

---
+++ JavaScript/Tutorials/data-types.js
@@ -0,0 +1,9 @@
+const readline = require('readline');
+
+i2 = parseInt(readLine(), 10);
+d2 = parseFloat(readLine());
+s2 = readLine();
+
+console.log(i + i2);
+console.log((d + d2).toFixed(1));
+console.log(s + s2);
